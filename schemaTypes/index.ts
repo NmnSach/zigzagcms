00a9b859cfa61fe0eb6797f776ab2e4409d524f2
NmnSach <nssachdeva2@gmail.com>
2024-09-30 15:18:19 +0530

    removed unused schemas

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,7 +1,4 @@
 import test from "./test"
-import director from "./director"
 import video from "./video"
-import client from "./client"
-import industry from "./industry"
 
-export const schemaTypes = [test, video, director, client, industry]
+export const schemaTypes = [test, video]
