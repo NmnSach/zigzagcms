542afb0d875a6f9632775d6db5f3449b2e46527d
NmnSach <nssachdeva2@gmail.com>
2024-10-14 01:50:41 +0530

    added meow/team to index

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,7 @@ import test from "./test"
 import video from "./video"
 import director from "./director"
 import producer from "./producer"
+import meow from "./meow"
+import team from "./team"
 
-export const schemaTypes = [test, video, director, producer]
+export const schemaTypes = [test, video, director, producer, meow, team]
